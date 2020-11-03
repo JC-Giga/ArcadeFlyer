85bdf66a78ca22f2824fe9b093a4f649be19dc29
JC-Giga <JC12525400@gmail.com>
2020-11-03 18:37:30 -0500

    Added Moving Projectiles

--- src/Player.cs
+++ src/Player.cs
@@ -13,6 +13,15 @@ namespace ArcadeFlyer2D
         // The speed at which the player can move
         private float movementSpeed = 4.0f;
 
+        // Min time between shots
+        private float projectileCoolDownTime = 0.5f;
+
+        private float projectileTimer = 0.0f;
+
+        private bool projectileTimerActive = false;
+        //current time since shot
+
+
         // Initialize a player                          _calls base class sprite below_
         public Player(ArcadeFlyerGame root, Vector2 position) : base(position)
         // call base class sprite
@@ -37,10 +46,11 @@ namespace ArcadeFlyer2D
         private void HandleInput(KeyboardState currentKeyboardState)
         {
             // Get all the key states
-            bool upKeyPressed = currentKeyboardState.IsKeyDown(Keys.Up);
-            bool downKeyPressed = currentKeyboardState.IsKeyDown(Keys.Down);
-            bool leftKeyPressed = currentKeyboardState.IsKeyDown(Keys.Left);
-            bool rightKeyPressed = currentKeyboardState.IsKeyDown(Keys.Right);
+            bool upKeyPressed = currentKeyboardState.IsKeyDown(Keys.W);
+            bool downKeyPressed = currentKeyboardState.IsKeyDown(Keys.S);
+            bool leftKeyPressed = currentKeyboardState.IsKeyDown(Keys.A);
+            bool rightKeyPressed = currentKeyboardState.IsKeyDown(Keys.D);
+            bool spaceKeyPressed = currentKeyboardState.IsKeyDown(Keys.Space);
 
             // If Up is pressed, decrease position Y
             if (upKeyPressed)
@@ -65,6 +75,19 @@ namespace ArcadeFlyer2D
             {
                 position.X += movementSpeed;
             }
+            // If Spapce is pressed, shoot a projectile
+            if (spaceKeyPressed && !projectileTimerActive)
+            {
+                Vector2 projectilePosition;
+                Vector2 projectileVelocity;
+
+                projectilePosition = new Vector2(position.X + (SpriteWidth/ 2), position.Y + (SpriteHeight / 2));
+                projectileVelocity = new Vector2(10.0f, 0.0f);
+                root.FireProjectile(projectilePosition, projectileVelocity);
+                projectileTimerActive = true;
+                projectileTimer = 0f;
+
+            }
         }
 
         // Called each frame
@@ -75,6 +98,16 @@ namespace ArcadeFlyer2D
 
             // Handle any movement input
             HandleInput(currentKeyboardState);
+
+            if (projectileTimerActive)
+            {
+                projectileTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (projectileTimer > projectileCoolDownTime)
+                {
+                    projectileTimerActive = false;
+                }
+            }
         }
     }
 }
